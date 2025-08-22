c03c4588301940f8f95755c70a677c89eee41413
riAnriAn0 <ml2463265@gmail.com>
2025-08-22 15:01:16 -0300

    finalizar projeto

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 // variaveis 
-
 let input = document.getElementById("amigo");
 let listaAmigos = document.getElementById("listaAmigos");
 let resultado = document.getElementById("resultado");
 let button = document.querySelector(".button-add");
+
 let amigos = [];
+let numerosSorteados = [];
+
+function limparInput(){
+    input.value = "";
+}
+
+function limparLista(){
+    listaAmigos.innerHTML = "";
+}
+
+function sortearNumero(lista) {
+    
+    if(numerosSorteados.length === lista.length){
+        return -1;
+    }
+
+    let num = parseInt(Math.random() * lista.length);
+    while (numerosSorteados.includes(num)) {
+        num = parseInt(Math.random() * lista.length);
+    }
+    numerosSorteados.push(num);
+    return num;
+}
 
 // atualizar a lista de amigos que sera exibidas
 function atualizarLista() {
@@ -20,6 +43,8 @@ function atualizarLista() {
 // adiciona amigo a lista
 function adicionarAmigo() {
     let nome = input.value.trim();
+    numerosSorteados = [];
+
     if (nome == "") {
         alert("Por favor, insira um nome válido.");
     }else if(amigos.includes(nome)){
@@ -29,6 +54,26 @@ function adicionarAmigo() {
         input.value = "";
         atualizarLista();
     }
+
+    input.focus();
 }
 
+function sortearAmigo(){
+    if (amigos.length < 2) {
+        alert("Adicione pelo menos dois amigos para sortear.");
+        return;
+    }
+
+    limparInput();
+
+    let index = sortearNumero(amigos);
+
+    if(index === -1) {
+        alert("Todos os amigos já foram sorteados.");
+    }else{
+        let amigoSorteado = amigos[index];
+        limparLista();
+        resultado.textContent = `O amigo secreto é: ${amigoSorteado}`;
+    }
+}
 
